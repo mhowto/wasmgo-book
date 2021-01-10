3ec610d67c4812fc5cb8025b1249a171616da1c3
mhowto <mhowto@gmail.com>
2021-01-10 20:25:15 +0800

    continue

--- code/rust/ch03/wasm-rust/src/binary/instructions.rs
+++ code/rust/ch03/wasm-rust/src/binary/instructions.rs
@@ -1,2 +1,29 @@
+use super::opcodes::*;
+use super::types::*;
+
 #[derive(Debug, Default)]
 pub struct Expr {}
+
+pub trait Instruction {
+    fn get_opcode(&self) -> Opcode;
+    fn get_opname(&self) -> &str {
+        OPNAMES[self.get_opcode() as usize]
+    }
+}
+
+enum BlockType {
+    Empty,
+    ValType(ValType),
+    TypeIdx(u32),
+}
+
+pub struct BlockInst {
+    block_type: BlockType,
+    insts: Vec<Box<Instruction>>,
+} 
+
+impl Instruction for BlockInst {
+    fn get_opcode(&self) -> Opcode {
+        Opcode::Block
+    } 
+}
